af2ef83abe0cc6b26d6df7dcc23abab4f450f541
Ekaterina Kireeva <e.radakina@gmail.com>
2019-12-03 23:18:29 +0300

    Add Description key to Card mock

--- src/mock/card.js
+++ src/mock/card.js
@@ -22,14 +22,33 @@ const MockCities = [
   `La Spezia`
 ];
 
+const MockDescription = (`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, 
+  non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius 
+  viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum 
+  sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu 
+  luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc 
+  fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`)
+  .replace(/\r?\n/g, ``)
+  .split(`.`);
+
 const getRandomPhotos = () => {
-  return new Array(getRandomInt(0, 10)).fill(`http://picsum.photos/300/150?r=${Math.random()}`);
+  return new Array(getRandomInt(0, 10))
+    .fill(`http://picsum.photos/300/150?r=${Math.random()}`);
+};
+
+const getRandomDescription = (paragraphs) => {
+  return new Array(getRandomInt(1, 3))
+    .fill(``)
+    .map(() => {
+      return paragraphs[getRandomInt(0, paragraphs.length - 1)];
+    }).join(`.`);
 };
 
 export const createCardData = () => {
   return {
     type: MockTypes[getRandomInt(0, MockTypes.length - 1)],
     city: MockCities[getRandomInt(0, MockCities.length - 1)],
-    photos: getRandomPhotos()
+    photos: getRandomPhotos(),
+    description: getRandomDescription(MockDescription)
   };
 };
