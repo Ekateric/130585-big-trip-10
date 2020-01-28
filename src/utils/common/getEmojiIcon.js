9d9c7294e83c85119f5bee08e152c9cbc632cb26
Ekaterina Kireeva <e.radakina@gmail.com>
2020-01-29 00:59:58 +0300

    Fix getEmojiIcon util

--- src/utils/common/getEmojiIcon.js
+++ src/utils/common/getEmojiIcon.js
@@ -5,7 +5,10 @@ export default (name) => {
   let icon = emoji[name];
 
   if (typeof icon === `undefined`) {
-    icon = emoji[getRandomInt(0, Object.keys(emoji).length - 1)];
+    const allEmojis = Object.keys(emoji);
+    const rndEmojiName = allEmojis[getRandomInt(0, allEmojis.length - 1)];
+
+    icon = emoji[rndEmojiName];
   }
 
   return icon;
